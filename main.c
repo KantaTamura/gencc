95529e213c5b077813ef187792563ef7dfb42808
KantaTamura <tkanta496@gmail.com>
2023-06-14 12:44:04 +0900

    :sparkles: add +,- operator

--- main.c
+++ main.c
@@ -6,11 +6,31 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
-    char *endptr = 0;
+
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("    mov rax, %ld\n", strtol(argv[1], &endptr, 10));
+    printf("    mov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("    add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("    sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+        return 1;
+    }
+
     printf("    ret\n");
     return 0;
 }
