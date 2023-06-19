a75ba58c07fc228763e38b1f0d0d7a3aa723079e
KantaTamura <tkanta496@gmail.com>
2023-06-19 17:31:16 +0900

    :bug: 識別子として'_'を認識するように組み込みにisalphaからis_alpha関数に変更

--- test.sh
+++ test.sh
@@ -65,6 +65,8 @@ assert 3 'return foo = 3;'
 assert 3 'return foo123 = 3;'
 assert 6 'foo = 1; bar = 2 + 3; return foo + bar;'
 
+assert 2 'foo_bar = 2; return foo_bar;'
+
 assert 3 'if (0) return 2; return 3;'
 assert 3 'if (1-1) return 2; return 3;'
 assert 2 'if (1) return 2; return 3;'
--- tokenize.c
+++ tokenize.c
@@ -26,13 +26,6 @@ void error_at(const char *loc, char *fmt, ...) {
     exit(1);
 }
 
-// 指定した長さの文字列をコピーする
-char *strndup(char *p, long len) {
-    char *buf = malloc(len + 1);
-    strncpy(buf, p, len);
-    buf[len] = '\0';
-    return buf;
-}
 // 次のトークンが期待している記号のときは，トークンを1つ読み進めて真を返す．
 // それ以外の場合には偽を返す．
 bool consume(char *op) {
@@ -118,6 +111,24 @@ char *startswith_keyword(char *p) {
     return NULL;
 }
 
+// 指定した長さの文字列をコピーする
+char *strndup(char *p, long len) {
+    char *buf = malloc(len + 1);
+    strncpy(buf, p, len);
+    buf[len] = '\0';
+    return buf;
+}
+
+// [a-z] or [A-Z] or '_' ならtrueを返す
+bool is_alpha(char c) {
+    return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
+}
+
+// [a-z] or [A-Z] or [0-9] or '_' ならtrueを返す
+bool is_alnum(char c) {
+    return is_alpha(c) || ('0' <= c && c <= '9');
+}
+
 // "user_input"をトークナイズしてそれを返す．
 Token *tokenize() {
     char *p = user_input;
@@ -153,9 +164,9 @@ Token *tokenize() {
             continue;
         }
 
-        if (isalpha(*p)) {
+        if (is_alpha(*p)) {
             char *q = p++;
-            while (isalnum(*p)) {
+            while (is_alnum(*p)) {
                 p++;
             }
             cur = new_token(TK_IDENT, cur, q, p - q);
